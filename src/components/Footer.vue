20970b3d5decb5737a046ce9b4a84b961c677a17
Reuben Mathew <intelphysic@gmail.com>
2020-04-11 16:27:58 -0400

    completed footer, lvl4 init

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,10 @@
 <template>
-    <div class="py-12 bg-lightgrey">
-        <!-- <div class="px-2">
-        <div class="flex -mx-2">
-            <div class="w-1/3 px-2">
-            <div class="bg-gray-400 h-12">
-                <span class="text-grey">Hello</span> 
-            </div>
-            </div>
-            <div class="w-1/3 px-2">
-            <div class="bg-gray-500 h-12"></div>
-            </div>
-            <div class="w-1/3 px-2">
-            <div class="bg-gray-400 h-12"></div>
-            </div>
+    <div class="h-auto flex text-sm bg-black md:px-12 p-3">
+        <div class="w-1/2 text-left">
+            <p class="text-white">Made with ❤️ by Reuben</p>
         </div>
-        </div> -->
-       
+        <div class=" w-1/2 text-right">
+            <p class="text-white">2020. <b>Lvl4 Production</b></p>
+        </div>   
     </div>
 </template>
