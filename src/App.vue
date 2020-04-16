a65239f59ad5e455e2c2cde1444681008396bd25
Reuben Mathew <intelphysic@gmail.com>
2020-04-15 22:27:53 -0400

    added contact section

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,41 @@
     <MainScreen/>
     <Banner/>
     <InfoContainer/>
-    <Footer class="mt-10"/>
-</div>
+
+    <div class="container bg-gray-100 max-w-full mx-auto px-6 py-12">
+      <div class="max-w-lg mx-auto px-6">
+          <div class="font-serif font-thin text-center text-black text-5xl">Contact Us</div>
+          <div class="max-w-xl shadow-md">
+            <form class=" bg-white rounded-lg px-8 pt-3 pb-8" action="https://formspree.io/xlewvwne" method="POST">
+              <div class="mb-4 mt-2">
+                <label class="block w-full text-gray-700 text-sm font-bold mb-2" for="username">
+                  Name
+                </label>
+                <input type="text" name="name" class="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your Name">
+              </div>
+              <div class="mb-4">
+                <label class="block w-full text-gray-700 text-sm font-bold mb-2" for="username">
+                  Email
+                </label>
+                <input type="email" name="_replyto" class="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your Email">
+              </div>
+              <div class="mb-4">
+                <label class="block w-full text-gray-700 text-sm font-bold mb-2" for="username">
+                  Your Message
+                </label>
+                <textarea class="w-full h-32 resize-none focus:outline-none focus:shadow-outline border rounded text-gray-700 py-2 px-3" name="message" placeholder="Your Message"></textarea>
+              </div>
+              <div class="">
+                <input type="submit" value="Send" class="transition ease-in-out duration-200 bg-blue hover:bg-white hover:text-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              </div>
+            </form>
+          </div>
+      </div>
+    </div>
+
+    <Footer/>
+
+  </div>
 
 </template>
 <style>
--- src/components/InfoContainer.vue
+++ src/components/InfoContainer.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="container py-32 flex-wrap lg:flex md:mt-16 flex-wrap mx-auto px-5 md:px-16">
+    <div class="container pt-24 pb-12 flex-wrap lg:flex md:mt-16 flex-wrap mx-auto px-5 md:px-16">
       <div class="bgimg1 lg:w-4/6 bg-cover bg-top h-auto text-white py-24 px-10"></div> 
       <div class="lg:w-2/6 bg-top flex-wrap h-auto text-black py-24 px-4">
         <p class="text-3xl font-bold font-serif">Do Business</p>
